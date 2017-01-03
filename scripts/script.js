2e50d59ec1a2311f268e42121966fdb4940cb7e5
bkarakus <burak@obilet.com>
2017-01-03 20:13:49 +0300

    Replace old code with real portfolio code created in codepen

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
+    $('li a').on('click', function () {
+        var hash = $(this)[0].hash;
 
-    alert();
-
+        $('html,body').animate({
+                scrollTop: $(hash).offset().top - 50
+            },
+            'slow');
+    });
 });
