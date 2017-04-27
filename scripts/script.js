7a4b90b0f8697f0657bd6fde9f2e050d5f552434
bkarakus <burak@obilet.com>
2017-04-27 12:03:15 +0300

    add larger image overlay

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,33 @@
 $(document).ready(function () {
-    $('li a').on('click', function () {
+    $('li:not(:last-child) a').on('click', function () {
         var hash = $(this)[0].hash;
 
         $('html,body').animate({
                 scrollTop: $(hash).offset().top - 50
             },
             'slow');
+    });
+
+    $('li:last-child a').on('click', function () {
+        alert('last child');
+
+        // $('html,body').animate({
+        //         scrollTop: $(hash).offset().top - 50
+        //     },
+        //     'slow');
+    });
+
+    var i = 0;
+    $(".portfolio img")
+        .click(function () {
+            $('.overlay img').attr('src', $(this).attr('src'));
+
+            $('.overlay').show('slow');
+        });
+
+    $('.close').click(function () {
+
+        $('.overlay img').attr('src', '');
+        $('.overlay').hide('slow');
     });
 });
