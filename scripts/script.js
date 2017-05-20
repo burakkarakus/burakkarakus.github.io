49ae0e2b6e5288417e82bc91e133251a50f1ddc5
bkarakus <burak@obilet.com>
2017-05-20 20:14:14 +0300

    Fix body width problem

--- scripts/script.js
+++ scripts/script.js
@@ -8,15 +8,6 @@ $(document).ready(function () {
             'slow');
     });
 
-    $('li:last-child a').on('click', function () {
-        alert('last child');
-
-        // $('html,body').animate({
-        //         scrollTop: $(hash).offset().top - 50
-        //     },
-        //     'slow');
-    });
-
     var i = 0;
     $(".portfolio img")
         .click(function () {
